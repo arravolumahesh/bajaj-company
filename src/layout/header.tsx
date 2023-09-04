26799f05a54de208be162a4b311b66553a8eb2ea
SURAJ GUPTA <37866971+srgsid@users.noreply.github.com>
2023-09-04 10:56:46 +0530

    Update package-lock.json with new dependencies
    The change includes removing some dependencies that might no longer be needed and moving some to devDependencies. Focus is given on updating and optimizing the list of dependencies. A few dependencies like @emotion/server, @fontsource/roboto and @types/node have also been removed, likely as they aren't being used anymore. Some version updates for

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -11,15 +11,13 @@ import ListItemText from '@mui/material/ListItemText';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import CSLink from '@/commonComponents/link';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import {useRouter} from 'next/navigation';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import {ListItem} from "@mui/material";
+import {Container, ListItem} from "@mui/material";
 
 
 const menuData = [
