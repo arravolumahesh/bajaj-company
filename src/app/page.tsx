b99cd43387d60b1c23cc6e9cc8db035cfbd4f976
Akash Dasgupta <bloombergakash@gmail.com>
2023-09-05 14:55:45 +0530

    Add: Demo card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import {Button} from "@mui/material";
+import DemoCard from "@/commonComponents/commonSlider/demoCard";
+import { Button } from "@mui/material";
 
 const HomePage = () => {
-    return (
-        <div>
-            <Button>Home Page</Button>
-        </div>
-    );
+  return (
+    <div>
+      <Button>Home Page</Button>
+      <DemoCard />
+    </div>
+  );
 };
 
 export default HomePage;
