92adbedd1109bc30ac3bb7b3fd650a1272863997
SURAJ GUPTA <37866971+srgsid@users.noreply.github.com>
2023-09-06 09:37:02 +0530

    Remove EnhancedSwiper from HomePage
    EnhancedSwiper component was removed from the HomePage for performance optimization and to reduce unnecessary complexity. Homepage now only renders a single button, reducing loading times and providing a more streamlined user experience.
    
    In the previous version, the page was using the EnhancedSwiper that included a static slide with set data. This data was redundant and not being used elsewhere in the application, resulting in unnecessary resource consumption. After examining the use-cases, it was evident the page didn't require a swiper for it's intended function, leading to the removal decision. Future components can be added when additional functionality for HomePage is required.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,9 @@
 import {Button} from "@mui/material";
-import EnhancedSwiper, {EnhancedSwiperCard} from "@cc/enhanced-swiper";
 
 const HomePage = () => {
     return (
         <div>
             <Button>Home Page</Button>
-
-            <EnhancedSwiper
-                SlideComponent={EnhancedSwiperCard}
-                data={[{data: {title: "title", description: "description"}}, {
-                    data: {
-                        title: "title",
-                        description: "description"
-                    }
-                }, {data: {title: "title", description: "description"}}, {
-                    data: {
-                        title: "title",
-                        description: "description"
-                    }
-                }]}
-            />
         </div>
     );
 };
