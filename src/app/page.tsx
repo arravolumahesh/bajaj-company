9b7f2d8c17ce169f994ac08c85a7790d70be5d30
SURAJ GUPTA <37866971+srgsid@users.noreply.github.com>
2023-09-15 17:25:08 +0530

    Cleanup Page.tsx and fix link style in caseStudyCard.tsx
    Removed unnecessary imports and components from Page.tsx to simplify the file structure. Logo and MLink were not required on the page and caused clutter. Also, modified the MLink style in caseStudyCard.tsx to improve consistency across the app. Instead of specifying the typography and color on each use, the styling is now handled globally. This change reduces redundancy and makes the codebase easier to manage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-import MLink from "@cc/m-link";
 import { Stack } from "@mui/material";
 import AnimatedButton from "@cc/animated-button";
-import Logo from "@cc/logo";
 
 const HomePage = () => {
   return (
     <Stack p={4} bgcolor={"primary.main"} spacing={4}>
-      <MLink href={"/"} variant={"outlined"} color={"inherit"}>
-        Home Page
-      </MLink>
-      <Logo />
       <AnimatedButton href={"./"}>Animated Button</AnimatedButton>
     </Stack>
   );
