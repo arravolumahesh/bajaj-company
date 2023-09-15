a7d020f6593076215d20c0fbd47c6997fd732b14
Akash Dasgupta <bloombergakash@gmail.com>
2023-09-15 22:05:14 +0530

    Add: hero animation complete

--- src/components/home/heroSection.tsx
+++ src/components/home/heroSection.tsx
@@ -1,14 +1,19 @@
 "use client";
 import AnimatedButton from "@/commonComponents/animated-button";
-import { Collapse, Slide, Stack, Typography } from "@mui/material";
+import { Box, Collapse, Fade, Slide, Stack, Typography } from "@mui/material";
 import Image from "next/image";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import BajajLogo from "./images/Graphics.svg";
 import Banner from "./images/Group 427321830.svg";
 
 const HeroSection = () => {
   const [logoAnimate, setLogoAnimate] = useState(false);
   const [bannerAnimate, setBannerAnimate] = useState(false);
+  const [headerAnimate, setHeaderAnimate] = useState(false);
+  const [subtitleAnimate, setSubtitleAnimate] = useState(false);
+  const [buttonAnimate, setButtonAnimate] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const btnContainerRef = useRef<HTMLElement>(null);
   useEffect(() => {
     setLogoAnimate(true);
   }, []);
@@ -27,27 +32,69 @@ const HeroSection = () => {
           m: "50px auto 87px",
         }}
       >
-        <Stack>
-          <Typography
-            variant='h1'
-            color={"#fff"}
-            sx={{ fontWeight: 400, mb: 2 }}
+        <Stack sx={{ overflow: "hidden" }} ref={containerRef}>
+          <Slide
+            direction='up'
+            in={headerAnimate}
+            timeout={800}
+            easing={"ease-out"}
+            container={containerRef.current}
+            onTransitionEnd={() => setSubtitleAnimate(true)}
           >
-            TURNING POTENTIAL TO PROGRESS
-          </Typography>
-          <Typography variant='body1' color={"#fff"}>
-            The wheels of a self-reliant future are turning with equal
-            opportunities for all.
-          </Typography>
+            <Box>
+              <Fade in={headerAnimate} timeout={2000}>
+                <Typography
+                  variant='h1'
+                  color={"#fff"}
+                  sx={{ fontWeight: 400, mb: 2 }}
+                >
+                  TURNING POTENTIAL TO PROGRESS
+                </Typography>
+              </Fade>
+            </Box>
+          </Slide>
+
+          <Slide
+            direction='up'
+            in={subtitleAnimate}
+            timeout={800}
+            easing={"ease-out"}
+            container={containerRef.current}
+            onTransitionEnd={() => setButtonAnimate(true)}
+          >
+            <Box>
+              <Fade in={subtitleAnimate} timeout={1500}>
+                <Typography variant='body1' color={"#fff"}>
+                  The wheels of a self-reliant future are turning with equal
+                  opportunities for all.
+                </Typography>
+              </Fade>
+            </Box>
+          </Slide>
           <AnimatedButton
             sx={{
-              fontSize: "24px !important",
               mt: 4,
               width: "207px",
+              bgcolor: "rgba(255, 255, 255, 0.10)",
             }}
             href={"./"}
+            ref={btnContainerRef}
           >
-            View Initiatives
+            <Slide
+              direction='up'
+              in={buttonAnimate}
+              timeout={1000}
+              easing={"ease-out"}
+              container={btnContainerRef.current}
+            >
+              <Box>
+                <Fade in={buttonAnimate} timeout={2000}>
+                  <Typography fontSize={"24px !important"}>
+                    View Initiatives
+                  </Typography>
+                </Fade>
+              </Box>
+            </Slide>
           </AnimatedButton>
         </Stack>
         <Stack direction='row' alignItems={"center"} minWidth={600}>
@@ -73,6 +120,7 @@ const HeroSection = () => {
             sx={{
               clipPath: "polygon(0% 0%, 75% 0%, 100% 50%, 74% 100%, 0% 100%)",
             }}
+            onTransitionEnd={() => setHeaderAnimate(true)}
           >
             <Image src={Banner} alt='Video Banner' height={543} />
           </Collapse>
