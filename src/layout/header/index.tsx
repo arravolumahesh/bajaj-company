d52520cccfbef2c7763c010602d83cb3c3e7907e
SURAJ GUPTA <37866971+srgsid@users.noreply.github.com>
2023-09-18 16:34:31 +0530

    Add animation and responsive design to Logo and Header
    Introduced an animated version of the logo for better visual appeal by incorporating the framer-motion library. The LogoProps is updated to include 'sx' properties enabling a much more responsive logo adaptation for varying screen sizes, enhancing the mobile UX. Also updated the 'header' layout to accommodate the logo's varying dimensions.

--- src/layout/header/index.tsx
+++ src/layout/header/index.tsx
@@ -17,8 +17,20 @@ const Header = (props: HeaderProps) => {
   const { routes } = props;
   return (
     <AppBar sx={appbarSx} elevation={0} position={"relative"}>
-      <SectionWrapper direction={"row"} alignItems={"center"}>
-        <Logo />
+      <SectionWrapper
+        direction={"row"}
+        alignItems={"center"}
+        py={0}
+        height={"inherit"}
+      >
+        <Logo
+          SvgIconProps={{
+            sx: {
+              width: { xs: 71, md: 91, xl: 107 },
+              height: { xs: 40, md: 52, xl: 64 },
+            },
+          }}
+        />
         <Navigation
           sx={{
             ml: "auto",
@@ -36,7 +48,7 @@ const appbarSx: SxProps<Theme> = (theme) => {
   return {
     background: theme.palette.gradient.primary,
     color: "primary.contrastText",
-    height: 120,
+    height: { xs: 64, md: 92, xl: 120 },
     zIndex: theme.zIndex.drawer + 1,
   };
 };
