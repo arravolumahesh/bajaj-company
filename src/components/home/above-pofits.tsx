be2004b3eb574f430acdddec51de38139da57324
SURAJ GUPTA <37866971+srgsid@users.noreply.github.com>
2023-09-20 15:22:40 +0530

    Fix typo in component name above-pofits.tsx
    Corrected the function name in 'above-pofits.tsx' from 'AbovePofits' to 'AboveProfits'. This change improves the clarity of the component name by aligning it with the intended language of the project and avoiding potential confusion when interpreting the function purpose. It follows best practices for naming conventions in code and enhances overall readability.

--- src/components/home/above-pofits.tsx
+++ src/components/home/above-pofits.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/commonComponents/motion-elements";
 import { PlayCircle } from "@mui/icons-material";
 
-const AbovePofits = () => {
+const AboveProfits = () => {
   const [textTransition, setTextTransition] = useState(false);
 
   const headerTransition: MotionTypographyProps = {
@@ -89,8 +89,8 @@ const AbovePofits = () => {
   return (
     <Stack textAlign={"center"} maxWidth={1051} m={"auto"}>
       <MotionTypography
-        variant='h3'
-        color='white'
+        variant="h3"
+        color="white"
         fontWeight={400}
         {...headerTransition}
       >
@@ -104,7 +104,7 @@ const AbovePofits = () => {
         zIndex={1}
         {...videoBannerTransition}
       >
-        <Image src={videoImage} alt='Video Thumbnail' fill />
+        <Image src={videoImage} alt="Video Thumbnail" fill />
         <IconButton
           sx={{
             position: "absolute",
@@ -124,8 +124,8 @@ const AbovePofits = () => {
         </IconButton>
       </MotionBox>
       <MotionTypography
-        variant='h6'
-        color='white'
+        variant="h6"
+        color="white"
         fontWeight={400}
         {...descTransition}
       >
