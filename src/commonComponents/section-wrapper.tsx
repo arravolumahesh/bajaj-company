8e3c90860b4fb97afaccdedc6124506d2d8f9b6f
SURAJ GUPTA <37866971+srgsid@users.noreply.github.com>
2023-09-20 12:40:23 +0530

    Update SectionWrapper component structure
    Modified the structure of the SectionWrapper component to better align with the given design. Changed Stack's properties and added an inner Stack for content. It helps maintain content width and alignment while respecting padding for different screen sizes.

--- src/commonComponents/section-wrapper.tsx
+++ src/commonComponents/section-wrapper.tsx
@@ -3,17 +3,23 @@ import { Stack, StackProps } from "@mui/material";
 interface SectionWrapperProps extends StackProps {}
 
 const SectionWrapper = (props: SectionWrapperProps) => {
+    const { children, ...rest } = props;
   return (
-    <Stack
-      px={{
-        xs: 3,
-        md: 5,
-        xl: 8,
-      }}
-      py={3.5}
-      component={"section"}
-      {...props}
-    />
+    <Stack component={"section"} alignItems={"center"}>
+      <Stack
+        px={{
+          xs: 3,
+          md: 5,
+          xl: 8,
+        }}
+        py={3.5}
+        width={1}
+        maxWidth={(theme) => theme.breakpoints.values.xxl}
+        {...rest}
+      >
+        {children}
+      </Stack>
+    </Stack>
   );
 };
 
