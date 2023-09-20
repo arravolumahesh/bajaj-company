a0daf8810b47a5738f2ea2bdb1595c9b0770e870
SURAJ GUPTA <37866971+srgsid@users.noreply.github.com>
2023-09-20 12:44:57 +0530

    Add SectionProps to SectionWrapper component
    Extended SectionWrapperProps interface to include SectionProps, which allows additional flexibility when using this component. It enables users to pass additional or override the default props of the Stack component being used internally. The main motivation behind this change was to provide the facility to control section-based styles or props at the usage level. Moreover, this change respects our effort to maintain generic components with adjustable prop values.

--- src/commonComponents/section-wrapper.tsx
+++ src/commonComponents/section-wrapper.tsx
@@ -1,26 +1,34 @@
 import { Stack, StackProps } from "@mui/material";
 
-interface SectionWrapperProps extends StackProps {}
+interface SectionWrapperProps extends StackProps {
+  SectionProps: Omit<StackProps, "children">;
+}
 
 const SectionWrapper = (props: SectionWrapperProps) => {
-    const { children, ...rest } = props;
-  return (
-    <Stack component={"section"} alignItems={"center"}>
+    const { children, SectionProps, ...rest } = props;
+    return (
       <Stack
-        px={{
-          xs: 3,
-          md: 5,
-          xl: 8,
-        }}
-        py={3.5}
-        width={1}
-        maxWidth={(theme) => theme.breakpoints.values.xxl}
-        {...rest}
+        component={"section"}
+        alignItems={"center"}
+        height={1}
+        {...SectionProps}
       >
-        {children}
+        <Stack
+          px={{
+            xs: 3,
+            md: 5,
+            xl: 8,
+          }}
+          py={3.5}
+          width={1}
+          maxWidth={(theme) => theme.breakpoints.values.xxl}
+          height={"inherit"}
+          {...rest}
+        >
+          {children}
+        </Stack>
       </Stack>
-    </Stack>
-  );
+    );
 };
 
 export default SectionWrapper;
